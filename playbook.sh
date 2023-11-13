fa6aab62b958eac14ca7553c484a1ac7a7c7c249
vitorreiel <vitorreiel@hotmail.com>
2023-11-12 23:09:22 -0300

    fix: guide in the script

--- playbook.sh
+++ playbook.sh
@@ -7,7 +7,7 @@ pip install boto3 > /dev/null 2>&1
 ansible-galaxy collection install community.aws > /dev/null 2>&1
 echo -e "\033[1;32m- [ Dependências instaladas com Sucesso! ] \033[0m\n"
 
-echo -e "\n\033[1;34m- [ Bem vindo ao LabCyberAcademy ] \033[0m"
+echo -e "\n\033[1;34m- [ Bem vindo ao Lab Cyber Academy ] \033[0m"
 sleep 1
 echo -e "\n\033[1;35m- [ Digite: 1 - Para criar um cenário de treinamento em Segurança Cibernética. ] \033[0m"
 echo -e "\n\033[1;31m- [ Digite: 2 - Para deletar um cenário de treinamento já existente. ] \033[0m\n"
@@ -33,6 +33,7 @@ if [ "$inicial" = "1" ]; then
 
   if [ "$confirmacao" = "Sim" ] || [ "$confirmacao" = "SIM" ] || [ "$confirmacao" == "sim" ] || [ "$confirmacao" == "s" ] || [ "$confirmacao" == "S" ]; then
 
+    echo -e "\n\033[1;33m- [ Iniciando configurações da Infraestrutura. Aguarde! ] \033[0m\n"
     awk -v new_value_1="$aws_access_key" 'NR == 2 {print "aws_access_key: " new_value_1} NR != 2' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
     awk -v new_value_2="$aws_secret_key" 'NR == 3 {print "aws_secret_key: " new_value_2} NR != 3' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
     awk -v new_value_3="$aws_session_token" 'NR == 4 {print "aws_session_token: " new_value_3} NR != 4' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
@@ -58,6 +59,7 @@ elif [ "$inicial" = "2" ]; then
 
   if [ "$deletar" = "Sim" ] || [ "$deletar" = "SIM" ] || [ "$deletar" == "sim" ] || [ "$deletar" == "s" ] || [ "$deletar" == "S" ]; then
 
+    echo -e "\n\033[1;33m- [ Encerrando toda a Infraestrutura. Aguarde! ] \033[0m\n"
     awk -v new_value_1="$aws_access_key" 'NR == 2 {print "aws_access_key: " new_value_1} NR != 2' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
     awk -v new_value_2="$aws_secret_key" 'NR == 3 {print "aws_secret_key: " new_value_2} NR != 3' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
     awk -v new_value_3="$aws_session_token" 'NR == 4 {print "aws_session_token: " new_value_3} NR != 4' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
